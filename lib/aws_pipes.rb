a907fd379d77958b06b7de19d0bfcb237362b0d5
Joe Nelson <cred+github@begriffs.com>
2012-12-23 15:47:13 -0600

    Move aws credentialing to shared Module

--- lib/aws_pipes.rb
+++ lib/aws_pipes.rb
@@ -1,5 +1,33 @@
 require "aws_pipes/version"
+require 'trollop'
 
 module AwsPipes
-  # Your code goes here...
+  def AwsPipes.common_aws_options program_name, custom_banner
+    ::Trollop::options do
+      version "#{program_name} #{VERSION} (c) 2013 Joe Nelson"
+      banner <<-EOS
+#{custom_banner}
+
+You may provide Amazon authentication through these environment variables:
+AWS_ACCESS_KEY_ID - AWS access key id
+AWS_ACCESS_KEY    - AWS secret access key
+
+Additional options:
+EOS
+      opt :keyid, "AWS access key id", :type => :string
+      opt :key, "AWS secret access key", :type => :string
+    end
+  end
+
+  def AwsPipes.access_key_id opts
+    ENV['AWS_ACCESS_KEY_ID'] ||
+      opts[:keyid] ||
+      ::Trollop::die("Missing access key id")
+  end
+
+  def AwsPipes.secret_access_key opts
+    ENV['AWS_ACCESS_KEY'] ||
+      opts[:key] ||
+      ::Trollop::die("Missing secret access key")
+  end
 end
